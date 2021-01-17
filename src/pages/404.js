37457c8ab6ade8ddb69f9af539891c8a3dcf32b1
Will Kerry <mail@example.com>
2021-01-17 15:24:18 +0100

    Styling the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
+import RecentPodcastRoll from "../components/RecentPodcastRoll";
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="container is-max-desktop">
-      <h1 className="title">Error</h1>
-      <p>This page has not been found.</p>
-    </div>
-  </Layout>
-)
 
-export default NotFoundPage
+    <section class="hero is-primary is-bold is-medium">
+      <div class="hero-body">
+        <div class="container is-max-desktop">
+          <h1 class="title">Error</h1>
+          <h2 class="subtitle">This page has not been found.</h2>
+        </div>
+      </div>
+    </section>
+    <section className="section">
+      <div className="container is-max-desktop">
+        <RecentPodcastRoll />
+        
+      </div>
+    </section>
+  </Layout>
+);
+
+export default NotFoundPage;
