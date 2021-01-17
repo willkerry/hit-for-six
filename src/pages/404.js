6cc542745551b74a1e5edb2ca14fe8c843c7b853
Will Kerry <mail@example.com>
2021-01-17 16:38:43 +0100

    Further tweaks to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,33 @@
 import React from "react";
 import Layout from "../components/Layout";
+import ErrorLogo from "../components/error-logo";
 import RecentPodcastRoll from "../components/RecentPodcastRoll";
+
+import Link from "gatsby-link";
 
 const NotFoundPage = () => (
   <Layout>
-
     <section class="hero is-primary is-bold is-medium">
       <div class="hero-body">
         <div class="container is-max-desktop">
-          <h1 class="title">Error</h1>
-          <h2 class="subtitle">This page has not been found.</h2>
+          <div className="level">
+            <div className="level-left">
+              <div className="level-item">
+                <ErrorLogo height="3em" width="auto" />
+              </div>
+              <div className="level-item has-text-centered-touch">
+                <div>
+                  <h2 class="subtitle">This page has not been found. <Link style={{ textDecoration: "underline" }} to="/">Home</Link>.</h2>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
     <section className="section">
       <div className="container is-max-desktop">
         <RecentPodcastRoll />
-        
       </div>
     </section>
   </Layout>
